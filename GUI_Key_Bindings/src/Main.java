b15497df160052946040b210a145b26dc60dcd53
d-tidwell <conversations.and.l@gmail.com>
2022-08-19 08:25:46 -0600

    drag and drop done

---
+++ GUI_Key_Bindings/src/Main.java
@@ -0,0 +1,18 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// Key Bindings - bind an Action to a KeyStroke
+//						 Don't require you to click a component to give it focus
+//						 all Swing components use Key Bindings
+//						 increase flexibility compared to KeyListener
+//						 can assign key strokes to individual Swing components
+//						 more difficult to use and set up : (
+		
+		Game game = new Game();
+		
+		
+
+	}
+
+}
